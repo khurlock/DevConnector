f0c544deab39a188c7f4dcaff316f7f734a69e0b
Kolin Hurlock <hurlock.kolin@gmail.com>
2018-06-20 04:02:00 -0700

    continued work on user login validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://brad:brad1234@ds163330.mlab.com:63330/samplemernproject'
+  mongoURI: 'mongodb://brad:brad1234@ds163330.mlab.com:63330/samplemernproject',
+  secretOrKey: 'secret'
 };
